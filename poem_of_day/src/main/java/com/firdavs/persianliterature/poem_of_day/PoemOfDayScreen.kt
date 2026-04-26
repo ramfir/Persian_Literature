3e56a5c53ac3282c526232f14749c466217ad764
mac <firdavs_20111@mail.ru>
2026-04-26 12:48:41 +0300

    fix: add LaunchedEffect import

--- poem_of_day/src/main/java/com/firdavs/persianliterature/poem_of_day/PoemOfDayScreen.kt
+++ poem_of_day/src/main/java/com/firdavs/persianliterature/poem_of_day/PoemOfDayScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
